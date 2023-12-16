2392d7f083bd9e3e21a05162a6dc6b9155479ba5
KIRTY GUPTA <146505239+kg-alt-01@users.noreply.github.com>
2023-12-16 14:05:51 +0530

    Create 3MaxOfThreeNos.py

---
+++ FOD/3MaxOfThreeNos.py
@@ -0,0 +1,14 @@
+# Printing the max of three number
+
+lst = []
+
+for i in range(3):
+    num = int(input(f"Enter Number {i+1} : "))
+    lst.append(num)
+
+max = lst[0]
+for i in range(3):
+    if lst[i] > max:
+        max=lst[i]
+
+print(f"Maximum Number = {max}")
